227f26c88d08cf06f03840a552be629be9344407
Artem Alimov <artemalimov33@gmail.com>
2023-03-04 14:22:37 +0200

    Add Config db

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 
+using System.Reflection;
 using Core.Entities;
 
 using Microsoft.EntityFrameworkCore;
@@ -14,5 +15,10 @@ namespace Infrastructure.Data
         public DbSet<Product> Products {get;set;}
         public DbSet<ProductBrand> ProductBrand { get; set; }
         public DbSet<ProductType> ProductType { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
